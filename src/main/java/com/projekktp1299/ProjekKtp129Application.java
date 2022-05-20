90203aa4a667ad0b798c1fd0f7c1dc8e1c7a051e
rafiffuaddo <rafiffuad3108@gmail.com>
2022-05-20 19:14:56 +0700

    first commit

---
+++ src/main/java/com/projekktp1299/ProjekKtp129Application.java
@@ -0,0 +1,13 @@
+package com.projekktp1299;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekKtp129Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekKtp129Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projekktp1299/ProjekKtp129ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekktp1299;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjekKtp129ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
